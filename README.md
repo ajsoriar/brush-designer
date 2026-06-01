897ed373effea60bf7fa287c10c8e5d9855d3c08
asr <ajsoriar@gmail.com>
2026-06-01 22:58:00 +0200

    Add Vite demo app setup

--- README.md
+++ README.md
@@ -4,7 +4,31 @@ A simple editor for creating and testing brushes in the browser.
 
 ## Demo
 
-Open any file in [demo](./demo) in your browser to see the component in action.
+Install dependencies first:
+
+```sh
+npm install
+```
+
+Run the main app locally with Vite:
+
+```sh
+npm run dev
+```
+
+You can also open any file in [demo](./demo) in your browser to see individual component demos.
+
+Generate a production build in `dist`:
+
+```sh
+npm run build
+```
+
+Preview the generated build:
+
+```sh
+npm run preview
+```
 
 ![brush-designer example](./demo/Screen-Shot-1.png?raw=true "brush-designer example")
 
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import $ from "jquery";
+
+import "./components/jqbrushdesigner/jqbrushdesigner.css";
+import "./components/bigColorPicker/bigColorPicker.css";
+import "./components/paintBoard/paintBoard.css";
+import "./components/paintTools/paintTools.css";
+import "./components/simpleColorPicker/simpleColorPicker.css";
+import "./components/simpleLineWidthPicker/simpleLineWidthPicker.css";
+import "./components/windowsManager/windowsManager.css";
+import "./components/newDocumentDialog/newDocumentDialog.css";
+import "./components/starGenerator/starGenerator.css";
+import "./toolsBar.css";
+
+window.$ = $;
+window.jQuery = $;
+
+await import("./components/jqbrushdesigner/jqbrushdesigner.js");
+await import("./components/bigColorPicker/bigColorPicker.js");
+await import("./components/paintBoard/paintBoard.js");
+await import("./components/paintTools/paintTools.js");
+await import("./components/simpleColorPicker/simpleColorPicker.js");
+await import("./components/simpleLineWidthPicker/simpleLineWidthPicker.js");
+await import("./components/windowsManager/windowsManager.js");
+await import("./components/newDocumentDialog/newDocumentDialog.js");
+await import("./components/starGenerator/starGenerator.js");
+await import("./app.js");
+await import("./zoom.js");
